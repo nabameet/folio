87aed70d77d5554e478e33311d770ca6b688f354
nabameet <thenabameet@gmail.com>
2023-01-01 21:35:57 +0530

    marked /chai as 'under construction'

--- src/routes/chai/+page.svelte
+++ src/routes/chai/+page.svelte
@@ -5,25 +5,33 @@
 	$: costOfChai = noOfChai * costOfOneChai;
 </script>
 
-<div class="justify-center w-full min-h-screen bg-black">
+<div class="min-h-screen bg-black">
 	<h1 class="font-bold text-9xl" style="-webkit-text-stroke: 1px white; paint-order: stroke fill">
-		<button
-			on:click={() => {
-				noOfChai--;
-			}}
-			class={noOfChai > 1 ? 'visible' : 'invisible'}
-			style="-webkit-text-stroke: 1px white; paint-order: stroke fill"
-			>-
-		</button>
-		buy me {noOfChai === 1 ? 'a' : noOfChai} chai
-		<button
-			on:click={() => {
-				noOfChai++;
-			}}
-			class=""
-			style="-webkit-text-stroke: 1px white; paint-order: stroke fill"
-			>+
-		</button>
+		under construction
 	</h1>
-	<h2 class="font-bold text-white text-7xl">₹{costOfChai}</h2>
 </div>
+
+<!--
+	<div class="justify-center w-full min-h-screen bg-black">
+		<h1 class="font-bold text-9xl" style="-webkit-text-stroke: 1px white; paint-order: stroke fill">
+			<button
+				on:click={() => {
+					noOfChai--;
+				}}
+				class={noOfChai > 1 ? 'visible' : 'invisible'}
+				style="-webkit-text-stroke: 1px white; paint-order: stroke fill"
+				>-
+			</button>
+			buy me {noOfChai === 1 ? 'a' : noOfChai} chai
+			<button
+				on:click={() => {
+					noOfChai++;
+				}}
+				class=""
+				style="-webkit-text-stroke: 1px white; paint-order: stroke fill"
+				>+
+			</button>
+		</h1>
+		<h2 class="font-bold text-white text-7xl">₹{costOfChai}</h2>
+	</div>
+-->
