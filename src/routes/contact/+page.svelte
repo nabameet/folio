e63050a3771a93739d10303faf8cd284436b9682
nabameet <thenabameet@gmail.com>
2023-06-12 19:12:22 +0530

    fixed netlify forms

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -11,11 +11,10 @@
 		data-netlify="true"
 		class="max-w-screen-xl flex flex-col w-full"
 	>
+		<input type="hidden" name="form-name" value="contact" />
 		<Input type="text" name="name" label="name" />
 		<Input type="email" name="email" label="email" />
 		<TextArea name="message" label="your message" />
 		<Button text="send" />
 	</form>
 </div>
-
-<input type="hidden" name="form-name" value="contact" />
