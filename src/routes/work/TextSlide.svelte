324f64ea9791c7168795c048a0beb0b65f0b8261
nabameet <thenabameet@gmail.com>
2025-01-03 14:06:19 +0530

    modified text style

--- src/routes/work/TextSlide.svelte
+++ src/routes/work/TextSlide.svelte
@@ -4,13 +4,13 @@
 </script>
 
 <section
-	class="flex flex-col pt-10 pb-10 content-center break-words justify-center w-full bg-black"
+	class="flex flex-col sm:flex-row gap-10 sm:gap-20 px-5 py-10 sm:py-52 content-center break-words justify-center w-full bg-black"
 >
 	<h1
-		class="mb-6 sm:mb-16 font-bold text-justify text-transparent text-5xl sm:text-9xl"
+		class="tracking-tighter flex-grow font-bold text-transparent text-white text-5xl sm:text-8xl"
 		style="-webkit-text-stroke: 1.5px white; paint-order: stroke fill"
 	>
 		{title}
 	</h1>
-	<p class="font-serif sm:text-2xl text-justify break-words text-white">{paragraph}</p>
+	<p class="font-serif sm:text-xl text-justify break-words text-white">{paragraph}</p>
 </section>
