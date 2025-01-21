241fe49adfb8b5f68552561da0b43b7eee2af98b
nabameet <thenabameet@gmail.com>
2025-01-21 12:39:10 +0530

    modified style of endslide

--- src/routes/work/EndSlide.svelte
+++ src/routes/work/EndSlide.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <section
-	class="flex flex-col content-center break-words justify-center w-full min-h-screen pt-10 pb-10 bg-black"
+	class="flex flex-col content-center break-words justify-center w-full min-h-screen py-10 px-5 bg-black"
 >
 	<h1
 		class="font-bold text-justify text-transparent text-5xl sm:text-9xl"
